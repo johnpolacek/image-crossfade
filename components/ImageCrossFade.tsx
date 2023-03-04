1d421562eac32fa9c7167feb45b369c2c9faa604
John Polacek <johnpolacek@gmail.com>
2023-03-04 16:30:18 -0600

    initial commit

---
+++ components/ImageCrossFade.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react"
+import Image from "next/image"
+import wait from "waait"
+
+const ImageCrossFade = ({ imgUrl }: { imgUrl: string }) => {
+  const [fadeIn, setFadeIn] = useState<boolean>(false)
+  const [loaded, setLoaded] = useState<boolean>(false)
+  const [loadedImage, setLoadedImage] = useState<string>(imgUrl)
+
+  useEffect(() => {
+    setLoaded(false)
+    setFadeIn(true)
+  }, [imgUrl])
+
+  return (
+    <div className="absolute top-0 left-0 w-full bg-black">
+      {fadeIn && (
+        <Image
+          onLoadingComplete={() => {
+            setLoaded(true)
+            wait(1100).then(() => {
+              console.log("setLoadedImage")
+              setLoadedImage(imgUrl)
+            })
+          }}
+          alt=""
+          src={imgUrl}
+          priority
+          width={1728}
+          height={864}
+          className={`w-full absolute top-0 left-0 z-10 transition-opacity duration-1000 ${loaded ? "opacity-100" : "opacity-0"
+            }`}
+        />
+      )}
+      <Image
+        onLoadingComplete={() => {
+          setFadeIn(false)
+        }}
+        alt=""
+        src={loadedImage}
+        priority
+        width={1728}
+        height={864}
+        className={`w-full transition-opacity duration-1000 ${fadeIn && !loaded ? "opacity-0" : "opacity-100"
+          }`}
+      />
+      <div
+        className="w-full h-full absolute left-0 bottom-0 z-10"
+        style={{
+          background:
+            "linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 50%)",
+        }}
+      ></div>
+    </div>
+  )
+}
+
+export default React.memo(ImageCrossFade)
